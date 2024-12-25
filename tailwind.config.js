74c92f23dba73b005bae503c9b670deb18bb68b3
PurnaCodes777 <purnacodes777@gmail.com>
2024-12-25 13:15:35 +0530

    fixed mobile responsiveness for mobiles

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       screens: {
-        'custom-range': { 'min': '428px', 'max': '768px' },
+        'custom-range': { 'min': '384px', 'max': '768px' },
         'tablet': { 'min': '769px', 'max': '1068px' },
         'mini-desktop': { 'min': '1068px', 'max': '1332px' },
       }
